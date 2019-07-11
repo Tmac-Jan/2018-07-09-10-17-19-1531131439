cbcd38faf07486053b561fe16a0788495f1378d2
zhanggi3 <gio.zhang@oocl.com>
2019-07-11 20:21:23 +0800

    feat: finish pratice05

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,26 @@
 package practice05;
 
 public class Person {
+
+  private String name;
+  private Integer age;
+
+  public Person() {
+  }
+
+  public Person(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
+  public String introduce(){
+    return "My name is "+name+". I am "+age+" years old. ";
+  }
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
 }
+
