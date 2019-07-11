b8720a70f95efb761f2d7ae7f8a49db35384787c
zhanggi3 <gio.zhang@oocl.com>
2019-07-11 20:20:30 +0800

    feat: finish pratice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,27 @@
 package practice02;
 
 public class Person {
+
+  private String name;
+  private Integer age;
+
+  public Person() {
+  }
+
+  public Person(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
+  public String introduce(){
+    return "My name is "+name+". I am "+age+" old.";
+  }
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,26 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+  private Integer klass;
+  public Student() {
+  }
+
+  public Student(String name, Integer age, Integer klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  @Override
+  public String introduce() {
+    return "I am a Student. I am at Class "+klass+".";
+  }
+
+  public Integer getKlass() {
+    return klass;
+  }
+
+  public void setKlass(Integer klass) {
+    this.klass = klass;
+  }
+
 }
